f09a5a01c807d923e206c1e41e417546ee8a2d7b
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-27 13:44:52 -0800

    Create Statistics.cs

---
+++ Languages/C#/gradebook/src/Gradebook/Statistics.cs
@@ -0,0 +1,13 @@
+namespace Gradebook {
+    public class Statistics {
+        public double Average;
+        public double High;
+        public double Low;
+
+        public Statistics(double Average, double High, double Low) {
+            this.Average = Average;
+            this.High = High;
+            this.Low = Low;
+        }
+    }
+}
