455ca4583b29a6ff956334adbe39fa6acef2c93a
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-26 18:30:30 -0800

    initial commit

---
+++ Languages/C#/gradebook/src/Gradebook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gradebook {
+    class Program {
+        static void Main (string[] args) {
+
+            double x = 34.1;
+            double y = 245.12;
+
+            Console.Beep ();
+
+            Console.WriteLine (x + y);
+
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
