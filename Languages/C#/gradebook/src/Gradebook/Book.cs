ed650521781e82b2cc389ba77321133d2b8ea04e
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-27 23:15:34 -0800

    Update Book.cs

--- Languages/C#/gradebook/src/Gradebook/Book.cs
+++ Languages/C#/gradebook/src/Gradebook/Book.cs
@@ -12,7 +12,9 @@ namespace Gradebook {
         }
 
         public void AddGrade(double grade) {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0) {
+                grades.Add(grade);
+            }
         }
 
         public Statistics GetStatistics() {
