6e386f33a0463976fd7c1cf23cfe65d7e4b6012b
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-27 17:53:25 -0800

    GetBookReturnsDifferentObjects

--- Languages/C#/gradebook/test/Gradebook.Tests/TypeTests.cs
+++ Languages/C#/gradebook/test/Gradebook.Tests/TypeTests.cs
@@ -4,6 +4,16 @@ using Xunit;
 namespace Gradebook.Tests {
     public class TypeTests {
         [Fact]
+        public void GetBookReturnsDifferentObjects() {
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
         public void TwoVarsCanReferenceSameObject() {
             var book1 = GetBook("Book 1");
             var book2 = book1;
