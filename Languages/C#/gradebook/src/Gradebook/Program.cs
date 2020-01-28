75901a3b8c501133a203fe8c1a3d9dab5d785474
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-27 23:58:33 -0800

    User inputs grades and can handle exceptions.

--- Languages/C#/gradebook/src/Gradebook/Program.cs
+++ Languages/C#/gradebook/src/Gradebook/Program.cs
@@ -7,17 +7,30 @@ namespace Gradebook {
 
             var book = new Book("Erik's Gradebook");
 
-            book.AddGrade(89.1);
-            book.AddGrade(12.5);
-            book.AddGrade(34.5);
-            book.AddGrade(78.32);
-            book.AddGrade(52.35);
+            while (true) {
+                Console.WriteLine("Please input a grade between 0 and 100");
+                var input = Console.ReadLine();
+
+                if (input == "q") {
+                    break;
+                }
+
+                try {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                } catch (ArgumentException ex) {
+                    Console.WriteLine(ex.Message);
+                } catch (FormatException ex) {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
 
             var stats = book.GetStatistics();
-
             Console.WriteLine($"Average score of grades is {stats.Average:N2}");
             Console.WriteLine($"Lowest score of grades is {stats.Low:N2}");
             Console.WriteLine($"Highest score of grades is {stats.High:N2}");
+            Console.WriteLine($"Student recieved an {book.Letter}");
 
         }
     }
