500ffe745f88f98a7130228e0100f78f7cbb0ef2
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-01 16:07:27 -0800

    Basic Folder Structure for Project and Tests

---
+++ Languages/C#/ACM/Tests/ACM.BLTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ACM.BLTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
