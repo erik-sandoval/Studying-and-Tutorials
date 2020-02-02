69ab8b0fcae696acba1675e9259f3fc917ebef3f
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-01 19:27:03 -0800

    Created tests for name, with or without one property, and a test for static.

--- Languages/C#/ACM/Tests/ACM.BLTest/CustomerTest.cs
+++ Languages/C#/ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -1,14 +1,48 @@
 using System;
 using Xunit;
 
-namespace ACM.BLTest
-{
-    public class CustomerTest
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace ACM.BLTest {
+    public class CustomerTest {
+        public override bool Equals(object obj) {
+            return base.Equals(obj);
+        }
 
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        [Fact]
+        public void TestName() {
+            var customer = new Customer { FirstName = "Bilbo", LastName = "Baggins" };
+
+            string expected = "Baggins, Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestNameAuth() {
+            var customer = new Customer { FirstName = "Bilbo", };
+
+            string expected = "Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StaticTest() {
+            var customer1 = new Customer { FirstName = "Bilbo" };
+            Customer.InstanceCount += 1;
+            var customer2 = new Customer { FirstName = "Frodo" };
+            Customer.InstanceCount += 1;
+            var customer3 = new Customer { FirstName = "Rosie" };
+            Customer.InstanceCount += 1;
+
+            Assert.Equal(3, Customer.InstanceCount);
         }
     }
 }
