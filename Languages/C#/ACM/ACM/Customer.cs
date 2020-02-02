046f07a2e5a24a18c0a1acaf89a2c55b619857fe
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-01 16:07:40 -0800

    Created Basic Customer Class

---
+++ Languages/C#/ACM/ACM/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ACM {
+    public class Customer {
+        public int customerId {
+            get;
+            private set;
+        }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName { get { return _lastName; } set { _lastName = value; } }
+
+        public string FullName {
+            get {
+                return $"{LastName},{FirstName}";
+            }
+        }
+    }
+}
