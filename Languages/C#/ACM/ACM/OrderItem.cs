84e92ac0d399199661219368402ad07d23e91cb2
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-02 17:03:56 -0800

    Update OrderItem.cs

--- Languages/C#/ACM/ACM/OrderItem.cs
+++ Languages/C#/ACM/ACM/OrderItem.cs
@@ -1,34 +1,34 @@
-using System.Collections.Generic;
-
-namespace ACM {
+namespace ACM.BL {
     public class OrderItem {
-
         public OrderItem() {
 
         }
-
         public OrderItem(int orderItemId) {
             OrderItemId = orderItemId;
         }
 
         public int OrderItemId { get; private set; }
-        public string Product { get; set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
         public int Quantity { get; set; }
-        public decimal? price { get; set; }
 
-        public List<Product> Retrieve() {
+        public OrderItem Retrieve(int orderItemId) {
 
-            return new List<Product>();
+            return new OrderItem();
         }
 
         public bool Save() {
+
             return true;
         }
 
         public bool Validate() {
             var isValid = true;
 
-            if (string.IsNullOrWhiteSpace(Product))isValid = false;
+            if (Quantity <= 0)isValid = false;
+            if (ProductId <= 0)isValid = false;
+            if (PurchasePrice == null)isValid = false;
+
             return isValid;
         }
     }
