dd05bd70a83a5c5ee58c53cea6e9b429e18d4e95
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-02 17:03:50 -0800

    Update Order.cs

--- Languages/C#/ACM/ACM/Order.cs
+++ Languages/C#/ACM/ACM/Order.cs
@@ -1,36 +1,23 @@
 using System;
-using System.Collections.Generic;
 
-namespace ACM {
+namespace ACM.BL {
     public class Order {
         public Order() {
 
         }
-
         public Order(int orderId) {
             OrderId = orderId;
         }
 
         public DateTimeOffset? OrderDate { get; set; }
-
         public int OrderId { get; private set; }
-
-        public List<Order> Retrieve() {
-
-            return new List<Order>();
-        }
-
-        public bool Save() {
-            return true;
-        }
 
         public bool Validate() {
             var isValid = true;
 
-            // if (string.IsNullOrWhiteSpace(OrderId))isValid = false;
+            if (OrderDate == null)isValid = false;
+
             return isValid;
         }
-
     }
-
 }
