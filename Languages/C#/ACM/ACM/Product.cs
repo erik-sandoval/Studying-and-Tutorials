ceec35b315a93e83dab3b57f3a5ef1fb7daf5e23
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-02 17:04:00 -0800

    Update Product.cs

--- Languages/C#/ACM/ACM/Product.cs
+++ Languages/C#/ACM/ACM/Product.cs
@@ -1,34 +1,22 @@
-using System.Collections.Generic;
-
-namespace ACM {
+namespace ACM.BL {
     public class Product {
-
         public Product() {
 
         }
-
         public Product(int productId) {
             ProductId = productId;
         }
 
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
         public int ProductId { get; private set; }
-        public decimal? price { get; set; }
-
-        public List<Product> Retrieve() {
-
-            return new List<Product>();
-        }
-
-        public bool Save() {
-            return true;
-        }
+        public string ProductName { get; set; }
 
         public bool Validate() {
             var isValid = true;
 
-            if (string.IsNullOrWhiteSpace(Name))isValid = false;
+            if (string.IsNullOrWhiteSpace(ProductName))isValid = false;
+            if (CurrentPrice == null)isValid = false;
 
             return isValid;
         }
