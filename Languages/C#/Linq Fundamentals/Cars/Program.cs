8cc4dbd11addb3a6b6e77fa2bdc5b2a7e6a7ac21
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-12 00:07:23 -0800

    Refactored allmans

--- Languages/C#/Linq Fundamentals/Cars/Program.cs
+++ Languages/C#/Linq Fundamentals/Cars/Program.cs
@@ -6,48 +6,41 @@ using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cars
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Cars {
+    class Program {
+        static void Main(string[] args) {
             CreateXml();
             QueryXml();
         }
 
-        private static void QueryXml()
-        {
-            var ns = (XNamespace)"http://pluralsight.com/cars/2016";
-            var ex = (XNamespace)"http://pluralsight.com/cars/2016/ex";
+        private static void QueryXml() {
+            var ns = (XNamespace) "http://pluralsight.com/cars/2016";
+            var ex = (XNamespace) "http://pluralsight.com/cars/2016/ex";
             var document = XDocument.Load("fuel.xml");
 
             var query =
                 from element in document.Element(ns + "Cars")?.Elements(ex + "Car")
-                                                       ?? Enumerable.Empty<XElement>()
+                                ?? Enumerable.Empty<XElement>()
                 where element.Attribute("Manufacturer")?.Value == "BMW"
                 select element.Attribute("Name").Value;
 
-            foreach (var name in query)
-            {
+            foreach (var name in query) {
                 Console.WriteLine(name);
             }
         }
 
-        private static void CreateXml()
-        {
+        private static void CreateXml() {
             var records = ProcessCars("fuel.csv");
 
-            var ns = (XNamespace)"http://pluralsight.com/cars/2016";
-            var ex = (XNamespace)"http://pluralsight.com/cars/2016/ex";
+            var ns = (XNamespace) "http://pluralsight.com/cars/2016";
+            var ex = (XNamespace) "http://pluralsight.com/cars/2016/ex";
             var document = new XDocument();
             var cars = new XElement(ns + "Cars",
-
                 from record in records
                 select new XElement(ex + "Car",
-                                new XAttribute("Name", record.Name),
-                                new XAttribute("Combined", record.Combined),
-                                new XAttribute("Manufacturer", record.Manufacturer))
+                    new XAttribute("Name", record.Name),
+                    new XAttribute("Combined", record.Combined),
+                    new XAttribute("Manufacturer", record.Manufacturer))
             );
 
             cars.Add(new XAttribute(XNamespace.Xmlns + "ex", ex));
@@ -56,10 +49,8 @@ namespace Cars
             document.Save("fuel.xml");
         }
 
-        private static List<Car> ProcessCars(string path)
-        {
+        private static List<Car> ProcessCars(string path) {
             var query =
-
                 File.ReadAllLines(path)
                     .Skip(1)
                     .Where(l => l.Length > 1)
@@ -68,35 +59,29 @@ namespace Cars
             return query.ToList();
         }
 
-        private static List<Manufacturer> ProcessManufacturers(string path)
-        {
+        private static List<Manufacturer> ProcessManufacturers(string path) {
             var query =
-                   File.ReadAllLines(path)
-                       .Where(l => l.Length > 1)
-                       .Select(l =>
-                       {
-                           var columns = l.Split(',');
-                           return new Manufacturer
-                           {
-                               Name = columns[0],
-                               Headquarters = columns[1],
-                               Year = int.Parse(columns[2])
-                           };
-                       });
+                File.ReadAllLines(path)
+                    .Where(l => l.Length > 1)
+                    .Select(l => {
+                        var columns = l.Split(',');
+                        return new Manufacturer {
+                            Name = columns[0],
+                            Headquarters = columns[1],
+                            Year = int.Parse(columns[2])
+                        };
+                    });
             return query.ToList();
         }
     }
 
-    public class CarStatistics
-    {
-        public CarStatistics()
-        {
+    public class CarStatistics {
+        public CarStatistics() {
             Max = Int32.MinValue;
             Min = Int32.MaxValue;
         }
-        
-        public CarStatistics Accumulate(Car car)
-        {
+
+        public CarStatistics Accumulate(Car car) {
             Count += 1;
             Total += car.Combined;
             Max = Math.Max(Max, car.Combined);
@@ -104,8 +89,7 @@ namespace Cars
             return this;
         }
 
-        public CarStatistics Compute()
-        {
+        public CarStatistics Compute() {
             Average = Total / Count;
             return this;
         }
@@ -115,19 +99,14 @@ namespace Cars
         public int Total { get; set; }
         public int Count { get; set; }
         public double Average { get; set; }
-
     }
 
-    public static class CarExtensions
-    {
-        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
-        {
-            foreach (var line in source)
-            {
+    public static class CarExtensions {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source) {
+            foreach (var line in source) {
                 var columns = line.Split(',');
 
-                yield return new Car
-                {
+                yield return new Car {
                     Year = int.Parse(columns[0]),
                     Manufacturer = columns[1],
                     Name = columns[2],
