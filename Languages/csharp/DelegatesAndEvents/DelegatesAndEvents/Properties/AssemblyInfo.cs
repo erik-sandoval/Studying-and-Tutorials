a158d7becdd4d8a0ab603098838faae58ec39b8a
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-18 22:57:27 -0800

    Finished module 2

--- Languages/csharp/DelegatesAndEvents/DelegatesAndEvents/Properties/AssemblyInfo.cs
+++ Languages/csharp/DelegatesAndEvents/DelegatesAndEvents/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ade7fdc-e3d0-4cf1-bcc1-d8e3ef25ba25")]
+[assembly: Guid("e3759f65-ab7c-460f-958d-d5d36ca259a9")]
 
 // Version information for an assembly consists of the following four values:
 //
