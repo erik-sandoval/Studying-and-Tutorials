ad70547162d2bbe2f9ecf2853159a52ffc593d6b
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-19 21:52:53 -0800

    Commiting to debug, List.cshtml not recognizing properties

--- Languages/csharp/OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ Languages/csharp/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using OdeToFood.Core;
+using System.Collections.Generic;
+
 
 namespace OdeToFood.Data {
     public interface IRestaurantData {
@@ -14,13 +14,15 @@ namespace OdeToFood.Data {
         public InMemoryRestaurantData() {
             restaurants = new List<Restaurant>() {
                 new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
-                new Restaurant {Id = 2, Name = "Sultan Bakery", Location = "California", Cuisine = CuisineType.Indian},
-                new Restaurant {Id = 3, Name = "Taco Shop", Location = "California", Cuisine = CuisineType.Mexican}
+                new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican}
             };
         }
 
         public IEnumerable<Restaurant> GetAll() {
-            return from r in restaurants orderby r.Name select r;
+            return from r in restaurants
+                orderby r.Name
+                select r;
         }
     }
 }
--- Languages/csharp/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ Languages/csharp/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,17 +1,24 @@
+using System.Collections;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants {
     public class List : PageModel {
-        private readonly IConfiguration _config;
+         readonly IConfiguration config;
+         readonly IRestaurantData restaurantData;
+        
         public string Message { get; set; }
+        public IEnumerable Restaurants { get; set; }
 
-        public List(IConfiguration config) {
-            _config = config;
+        public List(IConfiguration config, IRestaurantData restaurantData) {
+            this.config = config;
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet() {
-            Message = _config["Message"];
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
