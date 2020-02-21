46daaed314a2b93eb64694891537e60cffa7f78c
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-20 18:09:47 -0800

    Created Restaurant details page

---
+++ Languages/csharp/OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants {
+    public class Detail : PageModel {
+        private readonly IRestaurantData _restaurantData;
+        public Restaurant Restaurant { get; set; }
+
+        public Detail(IRestaurantData restaurantData) {
+            _restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId) {
+            Restaurant = _restaurantData.GetById(restaurantId);
+
+            if (Restaurant == null) {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
