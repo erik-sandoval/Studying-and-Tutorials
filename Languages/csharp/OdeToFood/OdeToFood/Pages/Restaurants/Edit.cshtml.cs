77d184a880e28447fb7e4266f70615415f06a16b
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-22 19:32:03 -0800

    Created Edit page and added button to edit items on list.

---
+++ Languages/csharp/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+using OdeToFood.Core;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OdeToFood {
+    public class EditModel : PageModel {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData,IHtmlHelper htmlHelper) {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+        public IActionResult OnGet(int restaurantId) {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = restaurantData.GetById(restaurantId);
+
+            if (Restaurant == null) {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
