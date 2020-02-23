1a69edc453e3d0ad45ac0c3d0215679601b3b1b6
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-23 14:51:59 -0800

    new refactoring setting

--- Languages/csharp/OdeToFood/OdeToFood/Pages/Error.cshtml.cs
+++ Languages/csharp/OdeToFood/OdeToFood/Pages/Error.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace OdeToFood.Pages {
-    [ResponseCache(Duration = 0,Location = ResponseCacheLocation.None,NoStore = true)]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel {
         public string RequestId { get; set; }
 
--- Languages/csharp/OdeToFood/OdeToFood/Startup.cs
+++ Languages/csharp/OdeToFood/OdeToFood/Startup.cs
@@ -20,12 +20,12 @@ namespace OdeToFood {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddSingleton<IRestaurantData,InMemoryRestaurantData>();
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app,IWebHostEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             } else {
