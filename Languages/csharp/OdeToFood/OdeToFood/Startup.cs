cfa553c8d5718c4373eec8b439c32c330dfb599a
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-23 23:18:35 -0800

    Created Db Context, setting db url in appsettings and created dbcontextpool to configure a sql server.

--- Languages/csharp/OdeToFood/OdeToFood/Startup.cs
+++ Languages/csharp/OdeToFood/OdeToFood/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +16,10 @@ namespace OdeToFood {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContextPool<OdeToFoodDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
             services.AddRazorPages();
         }
