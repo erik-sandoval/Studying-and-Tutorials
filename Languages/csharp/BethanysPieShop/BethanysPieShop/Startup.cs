2720ec99ccb0f7d31074eecb857d013337d185ec
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-06 00:34:22 -0800

    Added Scroped Repos and Map Controller Router, and some middleware

--- Languages/csharp/BethanysPieShop/BethanysPieShop/Startup.cs
+++ Languages/csharp/BethanysPieShop/BethanysPieShop/Startup.cs
@@ -1,3 +1,4 @@
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,11 @@ namespace BethanysPieShop {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -17,12 +23,13 @@ namespace BethanysPieShop {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=index}/{id?}");
             });
         }
     }
