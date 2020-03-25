3a5a9771fd735fd7a1365b93b760a4d80a94a648
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-25 11:31:45 -0700

    Added styles

--- Languages/javascript/rn-first-app/App.js
+++ Languages/javascript/rn-first-app/App.js
@@ -33,3 +33,31 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    justifyContent: "center"
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10
+  },
+  input: {
+    borderBottomColor: "black",
+    borderWidth: 1,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    width: 300
+  },
+  listItemContainer: {},
+  listItem: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    marginVertical: 10
+  }
+});
