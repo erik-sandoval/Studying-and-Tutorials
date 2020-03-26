2b4f27c2d26eddd9802883e5d3e8a2b2a355a3c8
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-25 19:37:47 -0700

    implement GoalInput and GoalItem Components into App.js

--- Languages/javascript/rn-first-app/App.js
+++ Languages/javascript/rn-first-app/App.js
@@ -1,43 +1,25 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  ScrollView
-} from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import GoalItem from "./components/goalItem.component";
+import GoalInput from "./components/goalInput.component";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = enteredText => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  const addGoalHandler = enteredGoal => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), goal: enteredGoal }
+    ]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        ></TextInput>
-        <Button title="ADD" onPress={addGoalHandler}></Button>
-      </View>
-      <ScrollView>
-        {courseGoals.map((goal, i) => (
-          <View key={i}>
-            <Text style={styles.listItem}>{goal}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <GoalInput addGoal={addGoalHandler} />
+      <FlatList
+        data={courseGoals}
+        renderItem={({ item }) => <GoalItem item={item} />}
+      ></FlatList>
     </View>
   );
 }
@@ -60,13 +42,5 @@ const styles = StyleSheet.create({
     paddingVertical: 4,
     paddingHorizontal: 10,
     width: 300
-  },
-  listItemContainer: {},
-  listItem: {
-    padding: 10,
-    backgroundColor: "#ccc",
-    borderColor: "black",
-    borderWidth: 1,
-    marginVertical: 10
   }
 });
