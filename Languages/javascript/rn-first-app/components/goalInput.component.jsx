00aacb7b1fc3e2870382cd771402730dcc42f33c
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-25 22:30:46 -0700

    added an add goal handler and input component is now in a modal. updated styles also.

--- Languages/javascript/rn-first-app/components/goalInput.component.jsx
+++ Languages/javascript/rn-first-app/components/goalInput.component.jsx
@@ -1,23 +1,46 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import { StyleSheet, TextInput, View, Button, Modal } from "react-native";
 
-const GoalInput = ({ addGoal }) => {
+const GoalInput = ({ addGoal, isVisible, cancelAddGoal }) => {
   const [enteredGoal, setEnteredGoal] = useState("");
 
   const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
   };
 
+  const addGoalHandler = () => {
+    addGoal(enteredGoal);
+    setEnteredGoal("");
+  };
+
+  const cancelGoalHandler = () => {
+    cancelAddGoal();
+    setEnteredGoal("");
+  };
+
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Course Goal"
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-      ></TextInput>
-      <Button title="ADD" onPress={() => addGoal(enteredGoal)}></Button>
-    </View>
+    <Modal visible={isVisible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        ></TextInput>
+        <View style={styles.buttonContainer}>
+          <View style={[styles.cancelButton, styles.button]}>
+            <Button
+              title="CANCEL"
+              color="red"
+              onPress={cancelGoalHandler}
+            ></Button>
+          </View>
+          <View style={styles.button}>
+            <Button title="ADD" onPress={addGoalHandler}></Button>
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
@@ -25,16 +48,29 @@ export default GoalInput;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 10
+    marginBottom: 10,
+    flex: 1
   },
   input: {
     borderBottomColor: "black",
+    marginBottom: 15,
     borderWidth: 1,
     paddingVertical: 4,
     paddingHorizontal: 10,
     width: 300
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    width: 300,
+    marginTop: 10
+  },
+  button: {
+    width: 100
+  },
+  cancelButton: {
+    marginRight: 20
   }
 });
