e1843052f057e404cb3fe99fa7feca88be7661ea
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-26 17:07:59 -0700

    initial app

---
+++ Languages/javascript/rn-complete-guide/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
