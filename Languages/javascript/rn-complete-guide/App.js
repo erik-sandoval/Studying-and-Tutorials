35328e10139a6d61d3f233a6fae5ff546f1fd14e
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-26 17:08:45 -0700

    implement header and startgame components

--- Languages/javascript/rn-complete-guide/App.js
+++ Languages/javascript/rn-complete-guide/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Header from "./component/header/header.component";
+import StartGameScreen from "./screens/start-game-screen.component";
 
 export default function App() {
   return (
     <View style={styles.screen}>
+      <Header title="Guess a Number"></Header>
+      <StartGameScreen></StartGameScreen>
     </View>
   );
 }
