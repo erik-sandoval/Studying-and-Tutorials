fa24348dc7e35cd75eeceb71005475ab02e6f7fe
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-29 17:56:58 -0700

    Initial game screen component

---
+++ Languages/javascript/rn-complete-guide/screens/game-screen.component.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import NumberContainer from "../component/card-containers/number-container.component";
+import Card from "../component/card/card.component";
+
+const GameScreen = ({ userChoice, onGameOver }) => {
+  return (
+  );
+};
+
+export default GameScreen;
+const styles = StyleSheet.create({
+});
