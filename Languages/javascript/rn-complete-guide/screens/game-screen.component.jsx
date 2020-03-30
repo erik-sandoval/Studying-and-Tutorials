5f8536ba6a1d99312bd9b99e1225aa8a091dcc83
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-29 17:57:34 -0700

    algo for generating number guess

--- Languages/javascript/rn-complete-guide/screens/game-screen.component.jsx
+++ Languages/javascript/rn-complete-guide/screens/game-screen.component.jsx
@@ -3,6 +3,19 @@ import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import NumberContainer from "../component/card-containers/number-container.component";
 import Card from "../component/card/card.component";
 
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
 const GameScreen = ({ userChoice, onGameOver }) => {
   return (
   );
