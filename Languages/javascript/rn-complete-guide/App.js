e7f0aeddbac75ef950493df9d5bd15121850c765
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-29 18:03:13 -0700

    handlers and hooks for app.js

--- Languages/javascript/rn-complete-guide/App.js
+++ Languages/javascript/rn-complete-guide/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./component/header/header.component";
 import StartGameScreen from "./screens/start-game-screen.component";
+import GameScreen from "./screens/game-screen.component";
+import GameOverScreen from "./screens/game-over-screen.component";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+
+  const configureNewGameHandler = () => {
+    setUserNumber(null);
+    setGuessRounds(0);
+  };
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"></Header>
