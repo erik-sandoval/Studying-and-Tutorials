501f3567f08daf9f4f673550c1cba30df93e9a18
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-30 20:05:32 -0700

    Loads font on initial load.

--- Languages/javascript/rn-complete-guide/App.js
+++ Languages/javascript/rn-complete-guide/App.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
 import Header from "./component/header/header.component";
 import StartGameScreen from "./screens/start-game-screen.component";
 import GameScreen from "./screens/game-screen.component";
 import GameOverScreen from "./screens/game-over-screen.component";
 
+const fetchFonts = () => {
+  Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
