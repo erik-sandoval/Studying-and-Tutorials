2101d7f9c94c1870848513c7e57512763e5c6f62
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-30 21:41:17 -0700

    Styled GameOver Screen and added an image.

--- Languages/javascript/rn-complete-guide/screens/game-over-screen.component.jsx
+++ Languages/javascript/rn-complete-guide/screens/game-over-screen.component.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, View, Button, Image, Text } from "react-native";
 import BodyText from "../component/styled-text/body-text.component";
 import TitleText from "../component/styled-text/title-text.component";
+import colors from "../constants/colors";
+import PrimaryButton from "../component/custom-buttons/primary-button.component";
 
 const GameOverScreen = ({ userNumber, roundsNumber, onRestart }) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
-      <BodyText>Number of rounds: {roundsNumber}</BodyText>
-      <BodyText>Number was: {userNumber}</BodyText>
-      <Button title="RESTART" onPress={onRestart}></Button>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/success.png")}
+          style={styles.image}
+        ></Image>
+      </View>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone took{" "}
+          <Text style={styles.textHightLight}>{roundsNumber}</Text>{" "}
+          {roundsNumber > 1 ? "rounds" : "round"} to guess the number{" "}
+          <Text style={styles.textHightLight}>{userNumber}</Text>.
+        </BodyText>
+      </View>
+      <PrimaryButton onPress={onRestart}>NEW GAME</PrimaryButton>
     </View>
   );
 };
@@ -21,5 +35,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderColor: "black",
+    borderWidth: 3,
+    marginVertical: 30,
+    overflow: "hidden"
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  textHightLight: {
+    color: colors.primary,
+    fontWeight: "700"
   }
 });
