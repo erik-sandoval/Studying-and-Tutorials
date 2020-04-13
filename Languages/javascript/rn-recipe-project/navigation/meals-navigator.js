90cd28cc999037455db2073136d76d1240dde2a4
Erik Sandoval <erik.sandoval@icloud.com>
2020-04-12 23:45:00 -0700

    added default header styles.

--- Languages/javascript/rn-recipe-project/navigation/meals-navigator.js
+++ Languages/javascript/rn-recipe-project/navigation/meals-navigator.js
@@ -3,11 +3,23 @@ import { createStackNavigator } from "react-navigation-stack";
 import CategoriesScreen from "../screens/categories.component";
 import CategoryMealsScreen from "../screens/category-meals.component";
 import MealDetailScreen from "../screens/meal-detail.component";
+import Colors from "../constants/Colors";
+import { Platform } from "react-native";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: CategoryMealsScreen,
-  MealDetail: MealDetailScreen,
-});
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: CategoryMealsScreen,
+    MealDetail: MealDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "#fff" : Colors.primaryColor,
+    },
+  }
+);
 
 export default createAppContainer(MealsNavigator);
