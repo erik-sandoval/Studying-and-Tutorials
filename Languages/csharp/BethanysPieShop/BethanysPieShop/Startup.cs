7798bb17312a3158b2548eba897eca94d4a73202
Erik Sandoval <erik.sandoval@icloud.com>
2020-06-18 01:49:40 -0700

    updated startup with shopping cart and session functionality

--- Languages/csharp/BethanysPieShop/BethanysPieShop/Startup.cs
+++ Languages/csharp/BethanysPieShop/BethanysPieShop/Startup.cs
@@ -25,6 +25,10 @@ namespace BethanysPieShop {
 
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+
             services.AddControllersWithViews();
 
         }
@@ -37,6 +41,7 @@ namespace BethanysPieShop {
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
