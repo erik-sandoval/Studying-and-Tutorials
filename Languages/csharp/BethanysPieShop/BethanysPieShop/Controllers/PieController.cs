87df0a6d66025d9e2d33630a678b66a21d835a41
Erik Sandoval <erik.sandoval@icloud.com>
2020-06-20 02:03:13 -0700

    Changed pie controller and added category menu in nav bar

--- Languages/csharp/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ Languages/csharp/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -1,6 +1,8 @@
 ﻿using BethanysPieShop.Models;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,13 +16,23 @@ namespace BethanysPieShop.Controllers {
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List() {
-            PiesListViewModel piesListViewModel = new PiesListViewModel {
-                Pies = _pieRepository.AllPies,
-                CurrentCategory = "Cheese Cakes"
-            };
+        public ViewResult List(string category) {
+            IEnumerable<Pie> pies;
+            string currentCategory;
 
-            return View(piesListViewModel);
+            if (string.IsNullOrEmpty(category)) {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            } else {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         public IActionResult Details(int id) {
