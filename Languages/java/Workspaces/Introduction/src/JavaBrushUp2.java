0f14eef61c4d318b82effaeb22217b82180371af
Erik Sandoval <erik.sandoval@icloud.com>
2021-12-04 20:52:42 -0800

    More Java Brush up

---
+++ Languages/java/Workspaces/Introduction/src/JavaBrushUp2.java
@@ -0,0 +1,16 @@
+
+public class JavaBrushUp2 {
+
+	public static void main(String[] args) {
+
+		int[] arr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int i = 0; i < arr2.length; i++) {
+
+			if (arr2[i] % 2 == 0) {
+				System.out.println(arr2[i]);
+			}
+		}
+	}
+
+}
