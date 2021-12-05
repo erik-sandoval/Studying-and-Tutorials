ca276f1d86abbf349bb9592fbcad6be2f3e428d9
Erik Sandoval <erik.sandoval@icloud.com>
2021-12-04 21:03:31 -0800

    Create MethodsDemo.java

---
+++ Languages/java/Workspaces/Introduction/src/MethodsDemo.java
@@ -0,0 +1,13 @@
+
+public class MethodsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void getData() {
+		System.out.println("hi");
+	}
+
+}
