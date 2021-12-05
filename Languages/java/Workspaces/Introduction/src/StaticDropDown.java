4d7714003000c8af823530c18aaa40bf5dc769a8
Erik Sandoval <erik.sandoval@icloud.com>
2021-12-04 21:03:12 -0800

    Create StaticDropDown.java

---
+++ Languages/java/Workspaces/Introduction/src/StaticDropDown.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StaticDropDown {
+
+	public static void main(String[] args) {
+
+	}
+
+}
