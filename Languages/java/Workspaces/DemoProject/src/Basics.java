324bf4e9ec09b239fe91fc870ee6709845d825ad
Erik Sandoval <48698089+erik-sandoval@users.noreply.github.com>
2022-01-07 00:12:44 -0800

    putting response of add place into a string variable

--- Languages/java/Workspaces/DemoProject/src/Basics.java
+++ Languages/java/Workspaces/DemoProject/src/Basics.java
@@ -9,10 +9,10 @@ public class Basics {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body(payload.AddPlace())
-				.when().post("maps/api/place/add/json").then().log().all().assertThat().statusCode(200)
-				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.18 (Ubuntu)");
+		String res = given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body(payload.AddPlace()).when().post("maps/api/place/add/json").then().assertThat().statusCode(200)
+				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.18 (Ubuntu)").extract().response()
+				.asString();
 	}
 
 }
