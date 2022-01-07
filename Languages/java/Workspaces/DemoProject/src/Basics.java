428334205af5bb2b5670d55c5058a39c318e4264
Erik Sandoval <48698089+erik-sandoval@users.noreply.github.com>
2022-01-07 00:13:37 -0800

    extracting place_id and updating address of place in a put request

--- Languages/java/Workspaces/DemoProject/src/Basics.java
+++ Languages/java/Workspaces/DemoProject/src/Basics.java
@@ -13,6 +13,16 @@ public class Basics {
 				.body(payload.AddPlace()).when().post("maps/api/place/add/json").then().assertThat().statusCode(200)
 				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.18 (Ubuntu)").extract().response()
 				.asString();
+
+		JsonPath js = new JsonPath(res);
+
+		String placeId = js.get("place_id");
+
+		given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body("{\n" + "\"place_id\":\"" + placeId + "\",\n" + "\"address\":\"70 Summer walk, USA\",\n"
+						+ "\"key\":\"qaclick123\"\n" + "}")
+				.when().put("maps/api/place/update/json").then().assertThat().statusCode(200)
+				.body("msg", equalTo("Address successfully updated"));
 	}
 
 }
