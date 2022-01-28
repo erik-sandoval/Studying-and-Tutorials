8cbc1c6cbd7050d1541f92808796cf3c66a790af
Erik Sandoval <48698089+erik-sandoval@users.noreply.github.com>
2022-01-28 12:29:14 -0800

    Iterate through json payload.

--- Languages/java/Workspaces/DemoProject/src/ComplexJsonParse.java
+++ Languages/java/Workspaces/DemoProject/src/ComplexJsonParse.java
@@ -4,6 +4,21 @@ import io.restassured.path.json.JsonPath;
 public class ComplexJsonParse {
 
 	public static void main(String[] args) {
+
+		JsonPath js = new JsonPath(payload.CoursePrice());
+
+		int count = js.getInt("courses.size()");
+
+		int purchaseAmount = js.getInt("dashboard.purchaseAmount");
+
+		for (int i = 0; i < count; i++) {
+			String title = js.get("courses[" + i + "].title");
+			int price = js.get("courses[" + i + "].price");
+
+			System.out.println(title + " price: $" + price + ".00");
+		}
+
+		System.out.println(purchaseAmount);
 	}
 
 }
