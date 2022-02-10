f548306f6d3c839ba863023dcc61cdf8fabdc032
Erik Sandoval <48698089+erik-sandoval@users.noreply.github.com>
2022-02-10 13:32:54 -0800

    add json method and created test case

--- Languages/Python/blog/tests/unit/post_test.py
+++ Languages/Python/blog/tests/unit/post_test.py
@@ -8,3 +8,8 @@ class PostTest(TestCase):
 
         self.assertEqual("test", p.title)
         self.assertEqual("test content", p.content)
+
+    def test_json(self):
+        p = Post("test", "test content")
+
+        self.assertDictEqual({"title": "test", "content": "test content"}, p.json())
