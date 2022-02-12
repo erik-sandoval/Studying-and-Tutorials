2ff69eed383dd4d7d7ba867617e02be9655d877a
Erik Sandoval <48698089+erik-sandoval@users.noreply.github.com>
2022-02-11 17:06:57 -0800

    reworked blog to take Post class objects

--- Languages/Python/blog/tests/blog.py
+++ Languages/Python/blog/tests/blog.py
@@ -1,3 +1,6 @@
+from tests.post import Post
+
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -5,7 +8,7 @@ class Blog:
         self.posts = []
 
     def create_post(self, title, content):
-        self.posts.append({"title": title, "content": content})
+        self.posts.append(Post(title, content))
 
     def __repr__(self):
         return f"{self.title} by {self.author} ({len(self.posts)} post{'s' if len(self.posts) > 1 else ''})"
@@ -14,5 +17,5 @@ class Blog:
         return {
             "title": self.title,
             "author": self.author,
-            "posts": self.posts
+            "posts": [post.json() for post in self.posts]
         }
